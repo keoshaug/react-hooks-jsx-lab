f340f4f2f330727eeed4c84a6ebe6760cc466e2e
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-08-09 13:01:06 -0400

    amending child components to app

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <>
+  <div id="about">About
+  <h2>About Me</h2>
+  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+  <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+  </div>
+  </>
+  );
+  
 }
 
 export default About;
